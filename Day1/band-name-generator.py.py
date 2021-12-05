83bc93dfd1e12663bb26a2806a1de4f59fddcacf
STEBELE <37588778+STEBELE@users.noreply.github.com>
2021-12-05 08:47:52 +0200

    Add files via upload

---
+++ Day1/band-name-generator.py.py
@@ -0,0 +1,10 @@
+# For this project it will be a band name generator from your name and that of your pet.
+# The skills and concepts ill be utilizing here today include working with variables, assigments, printing to the console, string manipulation, debugging and python input function(how to get the user to interact with the code).
+# 1. Create a greeting for your program.
+print("Welcome to the insane band name generator!")
+# 2. Ask the user for the city that they grew up in.
+user_name = input("What is your name? ")
+# 3. Ask the user for the name of a pet.
+pet_name = input("what is the name of your pet? ")
+# 4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be"+" " + user_name + " " + pet_name)
